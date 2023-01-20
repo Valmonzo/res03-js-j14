fa23307057d7a7a8b839c125bb2c1cdb27c4a830
Valmonzo <valmontpehautpietri@ip-172-31-26-63.eu-west-3.compute.internal>
2023-01-20 15:16:20 +0000

    projet perso js

--- projet-librairie/assets/js/classes/BookManager.js
+++ projet-librairie/assets/js/classes/BookManager.js
@@ -13,28 +13,94 @@ class BookManager {
         this.#books = books;
     }
 
-    findAllBooks();
-    findBooksById(id);
-    findBooksByTitle(title);
-    findBooksByAuthor(author);
-    findBooksByPublicationsYear(year);
-    createBook(book);
-    deleteBook(bookId);
-    editBook(book);
+    findAllBooks() {
+        return this.#books;
+    }
+
+    findBookById(id) {
+        for(let i = 0; i< this.#books.length; i++) {
+            if(this.#books[i].id === id) {
+                return this.#books[i];
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    findBooksByTitle(title) {
+
+        let booksTitle = [];
+        for(let i = 0; i< this.#books.length; i++) {
+            if(this.#books[i].title === title) {
+                return booksTitle.push(this.#books[i]);
+            }
+            else {
+                return null;
+            }
+
+    }
+
+    findBooksByAuthor(author) {
+        booksAuthor = [];
+        for(let i = 0; i< this.#books.length; i++) {
+            if(this.#books[i].author === author) {
+                return booksAuthor.push(this.#books[i]);
+            }
+            else {
+                return null;
+            }
+    }
+    findBooksByPublicationsYear(year) {
+        booksYear = [];
+        for(let i = 0; i< this.#books.length; i++) {
+            if(this.#books[i].publicationDate === year) {
+                return booksYear.push(this.#books[i]);
+            }
+            else {
+                return null;
+            }
+    }
+
+    createBook(book); {
+        this.#book.push(book);
+    }
+
+    deleteBook(bookId) {
+        newBooksTab =[];
+        for(let i = 0; i< this.#books.length; i++) {
+            if(this.#books[i].id !== bookId) {
+                newBooksTab.push(#books[i]);
+            }
+
+            this.#books = newBooksTab;
+    }
+
+
+    editBook(book) {
+        for(let i = 0; i< this.#books; i++) {
+            if(this.#books[i].id === book.id) {
+                this.#books[i] = book;
+            }
+        }
+    };
+
+
+
     save(){
 
         let stringBooks = JSON.stringify(this.#books);
-        sessionStorage.setItem("thisBooks", thisBooks);
+        sessionStorage.setItem("thisBooks", stringBooks);
 
-    };
+    }
     load() {
 
-        let loadBooks = [];
+
         let  parseBooks = JSON.parse(sessionStorage.getItem("thisBooks"));
         for (let i = 0; i < parseBooks.length; i++) {
         let parseData = JSON.parse(parseBooks[i]);
         let newBook = new Book(parseData.id, parseData.title, parseData.author, parseData.publicationDate, parseData.totalPages, parseData.excerpt, parseData.coverImage);
-        loadBooks.push(newBook);
+        this.#books.push(newBook);
     }
-    };
+    }
 }
--- projet-librairie/assets/js/classes/UserManager.js
+++ projet-librairie/assets/js/classes/UserManager.js
@@ -2,25 +2,87 @@ class UserManager {
     #users;
 
     constructor(users) {
-        this.#users = users;
+        this.#users = [];
     };
 
     get users() {
-        return this.#users
+        return this.#users;
     }
 
     set users(users) {
         this.#users = users;
     }
 
-    findAllUsers();
-    findUserById(id);
-    findUserByUsername(username);
-    findUserByEmail(email);
-    createUser(user);
-    deleteUser(userId);
-    editUser(user);
-    save();
-    load();
+    findAllUsers(){
+        return this.#users;
+    }
+
+    findUserById(id){
+        for(let i = 0; i< this.#users.length; i++) {
+            if(this.#users[i].id === id) {
+                return this.#users[i];
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    findUserByUsername(username) {
+        for(let i = 0; i< this.#users.length; i++) {
+            if(this.#users[i].username === username) {
+                return this.#users[i];
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    findUserByEmail(email){
+        for(let i = 0; i< this.#users.length; i++) {
+            if(this.#users[i].email === email) {
+                return this.#users[i];
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    createUser(user){
+        this.#users.push(user);
+    }
+
+    deleteUser(userId){
+        for(let i = 0; i< this.#users.length; i++) {
+            if(this.#users[i].id === userId) {
+                this.#users.remove(users[i]);
+            }
+    }
+
+    editUser(user){
+        newUserTab = [];
+        for(let i = 0; i< this.#users; i++) {
+            if(this.#users[i].id !== user.id) {
+                newUserTab.push(this.#users[i]);
+            }
+
+            this.#users = newUserTab;
+        }
+    }
+
+    save(){
+        let stringUsers = JSON.stringify(this.#users);
+        sessionStorage.setItem("thisUsers", stringUsers);
+    }
+
+    load() {
+        let  parseUsers = JSON.parse(sessionStorage.getItem("thisUsers"));
+        for (let i = 0; i < parseUsers.length; i++) {
+        let parseData = JSON.parse(parseUsers[i]);
+        let newUser = new User(parseData.id, parseData.username, parseData.email, parseData.password, parseData.firstName, parseData.lastName, parseData.profileImage);
+        this.#users.push(newUser);
+    }
 
 }
